64b6ef270bdd389b8ed6d61ac985a194d7953dd2
Nicholas Barbosa <nicholasbarbosads2001@gmail.com>
2023-10-30 21:32:05 -0300

    Página formulário, implementando PostMapping e Record.

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FilmeController {
     @GetMapping
     public String carregaPaginaFormulario() {
+        return "filmes/formulario";
+    }
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados) {
+        System.out.println(dados);
         return "filmes/formulario";
     }
 }
