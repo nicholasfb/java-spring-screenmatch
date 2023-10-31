e1b2e65d88d9003a10d94a4cddcae1dc039866c1
Nicholas Barbosa <nicholasbarbosads2001@gmail.com>
2023-10-31 20:57:58 -0300

    Criação da classe filmes, página de listagem e mapeando a página de listagem

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,21 +1,33 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
-    @GetMapping
+    private List<Filme> filmes = new ArrayList<>();
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario() {
         return "filmes/formulario";
     }
+    @GetMapping
+    public String carregaPaginaListagem() {
+        return "filmes/listagem";
+    }
     @PostMapping
     public String cadastraFilme(DadosCadastroFilme dados) {
-        System.out.println(dados);
+        var filme = new Filme(dados);
+        filmes.add(filme);
+
+        System.out.println(filmes);
         return "filmes/formulario";
     }
 }
