9c407773a5deb09b7fc11426628d4f3fdc5f9810
Nicholas Barbosa <nicholasbarbosads2001@gmail.com>
2023-11-01 21:21:20 -0300

    Utilizando a biblioteca Thymeleaf para iterar nossos elementos na página de listagem

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.controller;
 import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,8 @@ public class FilmeController {
         return "filmes/formulario";
     }
     @GetMapping
-    public String carregaPaginaListagem() {
+    public String carregaPaginaListagem(Model model) {
+        model.addAttribute("lista", filmes);
         return "filmes/listagem";
     }
     @PostMapping
